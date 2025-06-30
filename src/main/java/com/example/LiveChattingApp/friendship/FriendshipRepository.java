ce9571bce45e134f9c72755260ea4b543dd1afa9
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-06-30 13:49:56 +0200

    Updated repository to find pending friendships, blocked users, to check if there is a friendship between users etc.

--- src/main/java/com/example/LiveChattingApp/friendship/FriendshipRepository.java
+++ src/main/java/com/example/LiveChattingApp/friendship/FriendshipRepository.java
@@ -4,15 +4,55 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendshipRepository extends JpaRepository {
 
   @Query("""
     Select f from Friendship f
-    where (f.user.id = :userId and f.friend.id= :friendId)\s
-    or (f.user.id = :friendId and f.friend.id =:userId)
+    where (f.user.id = :usersId and f.friend.id= :friendId)\s
+    or (f.user.id = :friendId and f.friend.id =:usersId)
    \s""")
-  Optional<Friendship> friendshipBetweenUsers(@Param("userId") Integer userId,
+  Optional<Friendship> friendshipBetweenUsers(@Param("usersId") Integer usersId,
                                               @Param("friendId") Integer friendId);
+
+  boolean existsFriendshipBetweenUsers(@Param("usersId") Integer usersId,
+                                       @Param("friendId") Integer friendId);
+
+  @Query("""
+    select f from Friendship f
+    where (f.user.id= :usersId and f.friend.id= :friendId)
+    and f.friendshipsStatus = 'ACCEPTED'\s
+""")
+  List<Friendship> findAcceptedFriendships(@Param("usersId") Integer usersId,
+                                           @Param("friendId") Integer friendId);
+
+  @Query("""
+    select f from Friendship f\s
+    where(f.user.id =:usersId and f.friend.id= :friendId)
+    and f.friendshipsStatus = 'PENDING'
+""")
+    List<Friendship> findPendingFriendships(@Param("usersId") Integer usersId,
+                                            @Param("friendId") Integer friendId);
+
+    @Query("""
+    select f from Friendship f\s
+    where(f.user.id =:usersId and f.friend.id= :friendId)
+    and f.friendshipsStatus = 'BLOCKED'
+""")
+  List<Friendship> findBlockedUsers(@Param("usersId") Integer usersId,
+                                    @Param("friendId") Integer friendId);
+
+  @Query("""
+    select case when count(f) > 0 then true else false end from Friendship f where
+    (f.user.id= :userId and f.friend.id = :friendId) or
+    (f.user.id= :friendId and f.friend.id = :userId)
+""")
+
+  //To find requests to and from the user
+  List<Friendship> findRequestByUserIdAndStatus(Integer userId, FriendshipStatus status);
+
+  List<Friendship> findRequestByFriendIdAndStatus(Integer friendId, FriendshipStatus status);
+
 }
