de9bf589e7066be2730d82b5ef324c9de49a1d75
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-06-30 14:06:48 +0200

    Created friendship data transfer objects

---
+++ src/main/java/com/example/LiveChattingApp/friendship/FriendshipResponseDTO.java
@@ -0,0 +1,22 @@
+package com.example.LiveChattingApp.friendship;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendshipResponseDTO {
+
+  private Integer id;
+  private Integer userId;
+  private String username;
+  private String email;
+  private String status;
+  private String createdAt;
+  private boolean isRequester;
+
+}
