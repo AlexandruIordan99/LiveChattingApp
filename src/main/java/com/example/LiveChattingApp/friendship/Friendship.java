c78d98bd43933429c545fc2a8598b5f9f861d807
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-06-30 12:30:28 +0200

    Created friendship entity

---
+++ src/main/java/com/example/LiveChattingApp/friendship/Friendship.java
@@ -0,0 +1,39 @@
+package com.example.LiveChattingApp.friendship;
+
+
+import com.example.LiveChattingApp.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Friendship {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @ManyToOne
+  @JoinColumn(name = "users_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name="friend_id")
+  private User friend;
+
+  @Enumerated(EnumType.STRING)
+  private FriendshipStatus friendshipsStatus;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+}
