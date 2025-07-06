3ca022485f09ca8d4367e6cd834fc68bc2024769
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-06 20:57:23 +0200

    Added further dtesting depencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok:1.18.38")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+	testImplementation("org.testcontainers:postgresql:1.21.3")
 	testImplementation("org.mockito:mockito-core:2.1.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
