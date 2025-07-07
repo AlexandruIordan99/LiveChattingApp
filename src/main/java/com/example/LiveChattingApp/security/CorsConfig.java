d24e935ed65c3dc475819e530892928995d94882
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-07 18:31:01 +0200

    Updated headers and removed final backslash on localhost address

--- src/main/java/com/example/LiveChattingApp/security/CorsConfig.java
+++ src/main/java/com/example/LiveChattingApp/security/CorsConfig.java
@@ -2,6 +2,7 @@ package com.example.LiveChattingApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,9 +16,13 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200/")); // Angular app origin
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); // Angular app origin
+
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed HTTP methods
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Bearer")); // Allowed headers
+
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Bearer",
+          "Access-Control-Request-Method", "Access-Control-Request-Headers")); // Allowed headers
+
         config.setAllowCredentials(true); // Allow cookies and credentials
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config); // Apply CORS settings globally
