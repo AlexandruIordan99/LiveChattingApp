c21a10167905de883fa88bad3d3f7c9c47aeb48c
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-08 17:59:37 +0200

    Added oauth2 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	implementation("org.springframework.security:spring-security-oauth2-jose")
 	compileOnly("org.projectlombok:lombok:1.18.38")
 	annotationProcessor("org.projectlombok:lombok:1.18.38")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
