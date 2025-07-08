2f678108dc7563a85ab1df816afe8b2157eaad91
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-08 18:32:04 +0200

    User now extends base entity for created at and last modified date fields

--- src/main/java/com/example/LiveChattingApp/user/User.java
+++ src/main/java/com/example/LiveChattingApp/user/User.java
@@ -1,18 +1,16 @@
 package com.example.LiveChattingApp.user;
 
+import com.example.LiveChattingApp.common.BaseAuditingEntity;
 import com.example.LiveChattingApp.friendship.Friendship;
 import com.example.LiveChattingApp.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,7 +23,7 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
-public class User implements UserDetails, Principal {
+public class User extends BaseAuditingEntity implements UserDetails, Principal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,14 +46,6 @@ public class User implements UserDetails, Principal {
         inverseJoinColumns = @JoinColumn(name="users_id")
     )
     private List<Role> roles;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate; //we want to keep track of the user creation date and never update it
-    @LastModifiedDate
-    @Column(insertable = false) //when creating a new record, we do not want to initialize the value of this variable
-    private LocalDateTime modifiedDate;
-
 
     public String fullName(){
         return firstname + " " + lastname;
