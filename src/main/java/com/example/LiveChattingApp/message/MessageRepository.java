d1c5be5bd411f679355e1dce3abfe919432dd445
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-09 20:26:59 +0200

    Added set messages to seen

--- src/main/java/com/example/LiveChattingApp/message/MessageRepository.java
+++ src/main/java/com/example/LiveChattingApp/message/MessageRepository.java
@@ -1,8 +1,11 @@
 package com.example.LiveChattingApp.message;
 
+import com.example.LiveChattingApp.chat.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,5 +17,16 @@ public interface MessageRepository extends JpaRepository<Message, String> {
     order by message.createdDate
 """)
   Optional<Message> findMessageByChatId(@Param("chatId") String chatId);
+
+  @Transactional
+  @Modifying
+  @Query("""
+    update Message message
+    set message.state = :state
+    where message.chat.id = :chatId
+    
+""")
+  void setMessagesToSeen(@Param("chatId") Chat chat,
+                         @Param(" state") MessageState state);
 
 }
