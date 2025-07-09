b1b3b930f464a0ddc5436583dce9411740949834
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-09 18:59:56 +0200

    Created message response

---
+++ src/main/java/com/example/LiveChattingApp/message/MessageResponse.java
@@ -0,0 +1,25 @@
+package com.example.LiveChattingApp.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageResponse {
+
+  private Long id;
+  private String content;
+  private MessageType type;
+  private String senderId;
+  private String receiverId;
+  private LocalDateTime createdAt;
+  private byte[] media;
+
+
+}
