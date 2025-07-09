a0bcbc3624fe13c2e22d16997f9d9ef5137f07ee
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-09 18:56:15 +0200

    Created message service class

---
+++ src/main/java/com/example/LiveChattingApp/message/MessageService.java
@@ -0,0 +1,40 @@
+package com.example.LiveChattingApp.message;
+
+
+import com.example.LiveChattingApp.chat.Chat;
+import com.example.LiveChattingApp.chat.ChatRepository;
+import com.example.LiveChattingApp.user.User;
+import com.example.LiveChattingApp.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+  private final UserRepository userRepository;
+  private final MessageRepository messageRepository;
+  private final ChatRepository chatRepository;
+
+  public void saveMessage(MessageRequest messageRequest){
+    Chat chat = chatRepository.findById(messageRequest.getChatId())
+      .orElseThrow(() -> new RuntimeException("Chat not found"));
+
+    User sender = userRepository.findById(messageRequest.getSenderId())
+      .orElseThrow(() -> new RuntimeException("Sender not found"));
+
+    User receiver = userRepository.findById(messageRequest.getReceiverId())
+      .orElseThrow(() -> new RuntimeException("Receiver not found"));
+
+    Message message = Message.builder()
+      .content(messageRequest.getContent())
+      .chat(chat)
+      .sender(sender)
+      .receiver(receiver)
+      .type(messageRequest.getType())
+      .build();
+
+
+  }
+
+}
