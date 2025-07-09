518208b27c33da21e6c19476d4d2dbc541d3dd11
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-09 12:37:19 +0200

    Created message class

---
+++ src/main/java/com/example/LiveChattingApp/chat/Message.java
@@ -0,0 +1,42 @@
+package com.example.LiveChattingApp.chat;
+
+import com.example.LiveChattingApp.common.BaseAuditingEntity;
+import com.example.LiveChattingApp.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "messages")
+public class Message extends BaseAuditingEntity {
+
+  @Id
+  @SequenceGenerator(name = "msg_seq", sequenceName = "msg_seq", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "msg_seq")
+  private Integer id;
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  @ManyToOne(fetch =FetchType.LAZY)
+  @JoinColumn(name ="sender_id", nullable = false)
+  private User sender;
+
+  @ManyToOne(fetch =FetchType.LAZY)
+  @JoinColumn(name="receiver_id", nullable = false)
+  private User receiver;
+
+  private MessageState state;
+  private MessageType type;
+
+  @ManyToOne
+  @JoinColumn(name="chat_id")
+  private Chat chat;
+
+
+}
