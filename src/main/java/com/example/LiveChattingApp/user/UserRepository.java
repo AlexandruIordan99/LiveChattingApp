74316ba19438d9e94d199c0557e8f6b581253772
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-09 14:08:39 +0200

    Can now retrieve potential receivers and current users through ids

--- src/main/java/com/example/LiveChattingApp/user/UserRepository.java
+++ src/main/java/com/example/LiveChattingApp/user/UserRepository.java
@@ -1,11 +1,24 @@
 package com.example.LiveChattingApp.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);  //finds a user by their email
-                                                  //optional used to avoid null pointer errors role
+                                                  //optional used to avoid null pointer errors
+    @Query("""
+    select user from User user\s
+    where user.id != :userId
+""")
+    Optional<User> findAllUsersExceptSelf(@Param("userId") Integer userId);
+
+    @Query("""
+    select user from User user \s
+    where user.id == :userId
+""")
+    Optional<User> findUserById(@Param("userId") Integer userId);
 }
