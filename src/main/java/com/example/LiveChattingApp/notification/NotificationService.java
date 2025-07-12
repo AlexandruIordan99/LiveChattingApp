39495390ad33a2fb0b42cc1be09d5052737349aa
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-12 10:18:27 +0200

    Added error catching to send notif

--- src/main/java/com/example/LiveChattingApp/notification/NotificationService.java
+++ src/main/java/com/example/LiveChattingApp/notification/NotificationService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import com.example.LiveChattingApp.notification.Notification;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +12,16 @@ import com.example.LiveChattingApp.notification.Notification;
 public class NotificationService {
 
   private final SimpMessagingTemplate messagingTemplate;
-  public void sendNotification(String userId, Notification notification){
-    log.info("Sending WS notification to {} with payload {}", userId, notification);
-    messagingTemplate.convertAndSendToUser(
-      "topic/notification" + userId,
-      "/chat", notification);
-  }
 
+  public void sendNotification(String userId, Notification notification) {
+    try {
+      log.info("Sending WS notification to {} with payload {}", userId, notification);
+      messagingTemplate.convertAndSendToUser(
+        "topic/notification" + userId,
+        "/chat", notification);
+    } catch (Exception e) {
+      log.error("Failed to send notification to user {}: {}", userId, e.getMessage());
+    }
+
+  }
 }
