34d4bf41c43e6f36cc68047a2b6347f6faf908f5
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-15 18:02:56 +0200

    Updated message request repo to check for requests bi-directionally

--- src/main/java/com/example/LiveChattingApp/messageRequest/MessageRequestRepository.java
+++ src/main/java/com/example/LiveChattingApp/messageRequest/MessageRequestRepository.java
@@ -1,6 +1,7 @@
 package com.example.LiveChattingApp.messageRequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +12,15 @@ public interface MessageRequestRepository extends JpaRepository<MessageRequest, 
 
   Optional<MessageRequest> findById(MessageRequest messageRequest);
 
-
+  @Query("""
+      SELECT mr FROM MessageRequest mr
+        WHERE ((mr.senderId = :user1 AND mr.receiverId = :user2)
+           OR (mr.senderId = :user2 AND mr.receiverId = :user1))
+          AND mr.status = :status
+""")
   Optional<MessageRequest> findBySenderIdAndReceiverIdAndMessageRequestStatus(
     @Param("senderId") String senderId,
     @Param("receiverId") String receiverId,
-    @Param("messageRequestStatus") MessageRequestStatus messageRequestStatus);
+    @Param("messageRequestStatus") MessageRequestStatus status);
 
 }
