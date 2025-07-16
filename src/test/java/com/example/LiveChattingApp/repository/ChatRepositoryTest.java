da366b14dec96851567ae2fc6e20f06e20b50961
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-16 18:13:04 +0200

    Created chat repository tests

--- src/test/java/com/example/LiveChattingApp/repository/ChatRepositoryTest.java
+++ src/test/java/com/example/LiveChattingApp/repository/ChatRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.example.LiveChattingApp.repository;
 
+import com.example.LiveChattingApp.chat.Chat;
 import com.example.LiveChattingApp.chat.ChatRepository;
+import com.example.LiveChattingApp.chat.ChatType;
 import com.example.LiveChattingApp.user.User;
 import com.example.LiveChattingApp.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -13,6 +19,9 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @DataJpaTest
 @Testcontainers
@@ -38,7 +47,7 @@ public class ChatRepositoryTest {
   static void configureProperties(DynamicPropertyRegistry registry){
     registry.add("spring.datasource.url", postgres::getJdbcUrl);
     registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasoruce.password", postgres::getPassword);
+    registry.add("spring.datasource.password", postgres::getPassword);
   }
 
   @BeforeEach
@@ -86,7 +95,81 @@ public class ChatRepositoryTest {
 
   }
 
+  @Test
+   void test_doesDirectChatExist(){
+
+    //Arrange
+    Chat chat = Chat.builder()
+      .participants(Set.of(user1, user2))
+      .type(ChatType.DIRECT)
+      .build();
+
+    chatRepository.save(chat);
+
+    //Act
+
+    Optional<Chat> directChat = chatRepository.findDirectChatBetweenUsers(user1.getId(), user2.getId());
+
+    //Assert
+    Assertions.assertThat(directChat).isPresent();
+    Assertions.assertThat(directChat.get().getParticipants()).containsExactlyInAnyOrder(user1, user2);
+
+  }
+
+  @Test
+  void test_canFindChats(){
+    //Arrange
+    Chat directChat = Chat.builder()
+      .participants(Set.of(user1, user2))
+      .type(ChatType.DIRECT)
+      .build();
+
+    Chat groupChat =  Chat.builder()
+      .participants(Set.of(user1, user2, user3))
+      .type(ChatType.GROUP)
+      .build();
+
+    chatRepository.save(directChat);
+    chatRepository.save(groupChat);
+
+    //Act
+    List<Chat> user1Chats = chatRepository.findChatsByUserId(user1.getId());
+
+    //Assert
+    Assertions.assertThat(user1Chats).containsExactlyInAnyOrder(directChat, groupChat);
+  }
+
+  @Test
+  void test_ChatsWithMoreThan2ParticipantsAreGroupChats(){
+    //Arrange
+    Chat lyingGroupChat = Chat.builder()
+      .participants(Set.of(user1, user2, user3))
+      .type(ChatType.DIRECT)
+      .build();
+
+    //Act & Assert
+    Assertions.assertThatThrownBy(() -> chatRepository.save(lyingGroupChat))
+      .isInstanceOf(InvalidDataAccessApiUsageException.class)
+      .hasMessage("Direct chats must have exactly 2 participants.")
+      .hasCauseInstanceOf(IllegalArgumentException.class);
+
+  }
+
+  @Test
+  void test_ChatsWithLessThan3ParticipantsAreDirectChats(){
+    //Arrange
+    Chat lyingDirectChat = Chat.builder()
+      .participants(Set.of(user1, user2))
+      .type(ChatType.GROUP)
+      .build();
+
+    //Act & Assert
+    Assertions.assertThatThrownBy(() -> chatRepository.save(lyingDirectChat))
+      .isInstanceOf(InvalidDataAccessApiUsageException.class)
+      .hasMessage("Group chats must have at least 3 participants.")
+      .hasCauseInstanceOf(IllegalArgumentException.class);
 
 
+  }
 
 }
