6a9bcc17cda20704ac8e0aa685ec9e5210ea7d18
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-16 14:24:53 +0200

    Added process message request method

--- src/main/java/com/example/LiveChattingApp/messageRequest/MessageRequestService.java
+++ src/main/java/com/example/LiveChattingApp/messageRequest/MessageRequestService.java
@@ -1,6 +1,11 @@
 package com.example.LiveChattingApp.messageRequest;
 
 
+import com.example.LiveChattingApp.chat.ChatRepository;
+import com.example.LiveChattingApp.message.Message;
+import com.example.LiveChattingApp.message.MessageRepository;
+import com.example.LiveChattingApp.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +18,9 @@ import java.util.Optional;
 public class MessageRequestService {
 
   private final MessageRequestRepository messageRequestRepository;
+  private final UserRepository userRepository;
+  private final MessageRepository messageRepository;
+  private final ChatRepository chatRepository;
 
   public MessageRequest getOrCreateMessageRequest(MessageRequest request, String senderId, String receiverId) {
     Optional<MessageRequest> existingRequest =
@@ -36,7 +44,23 @@ public class MessageRequestService {
     return messageRequestRepository.save(newRequest);
   }
 
-  }
+  public void processMessageRequest(MessageRequest request, String chatId) {
+    if (request.getStatus() == MessageRequestStatus.ACCEPTED) {
+      Message message = new Message().builder()
+        .sender(userRepository.findById(request.getSenderId())
+          .orElseThrow(() -> new EntityNotFoundException("Sender not found.")))
+        .chat(chatRepository.findById(chatId)
+          .orElseThrow(() -> new EntityNotFoundException("Chat not found.")))
+        .content(request.getFirstMessages().toString())
+        .build();
 
+      messageRepository.save(message);
+      messageRequestRepository.delete(request);
+    } else if (request.getStatus() == MessageRequestStatus.DECLINED) {
+      messageRequestRepository.delete(request);
+    }
+
+  }
+}
 
 
