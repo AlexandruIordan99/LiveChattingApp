f477e46558efd70af7d9eb0077ea621dc47ecaf8
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-16 17:51:01 +0200

    Fixed existsDirectChat query

--- src/main/java/com/example/LiveChattingApp/chat/ChatRepository.java
+++ src/main/java/com/example/LiveChattingApp/chat/ChatRepository.java
@@ -17,14 +17,14 @@ public interface ChatRepository extends JpaRepository<Chat, String> {
                 where participants.id = :userId
                 order by chat.lastModifiedDate desc
 """)
-  List<Chat> findChatsByUserId(@Param("senderId") String userId);
+  List<Chat> findChatsByUserId(@Param("userId") String userId);
 
   @Query("""
        select chat from Chat chat
-               where chat.type = com.example.LiveChattingApp.chat.ChatType.DIRECT
-               and :user1Id member of chat.participants
-               and :user2Id member of chat.participants
-               and size(chat.participants) = 2
+       where chat.type = com.example.LiveChattingApp.chat.ChatType.DIRECT
+       and size(chat.participants) = 2
+       and exists (select 1 from chat.participants p1 where p1.id = :user1Id)
+       and exists (select 1 from chat.participants p2 where p2.id = :user2Id)
     """)
   Optional<Chat> findDirectChatBetweenUsers(@Param("user1Id") String user1Id,
                                             @Param("user2Id") String user2Id);
