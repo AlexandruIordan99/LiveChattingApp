e96f139abf8073de09a2ed6081010a2452434976
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-16 15:49:52 +0200

    Created initial set up

--- src/test/java/com/example/LiveChattingApp/repository/ChatRepositoryTest.java
+++ src/test/java/com/example/LiveChattingApp/repository/ChatRepositoryTest.java
@@ -1,11 +1,91 @@
 package com.example.LiveChattingApp.repository;
 
+import com.example.LiveChattingApp.chat.ChatRepository;
+import com.example.LiveChattingApp.user.User;
+import com.example.LiveChattingApp.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
 
 @DataJpaTest
 @Testcontainers
 public class ChatRepositoryTest {
+
+  @Autowired
+  private ChatRepository chatRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  private User user1;
+  private User user2;
+  private User user3;
+
+  @Container
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14")
+    .withDatabaseName("testDB")
+    .withUsername("postgres")
+    .withPassword("pass");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry){
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasoruce.password", postgres::getPassword);
+  }
+
+  @BeforeEach
+  void setUp(){
+
+    user1 = User.builder()
+      .firstname("Alexandru")
+      .lastname("Iordan")
+      .displayName("Jordan299")
+      .password("alunemari1234")
+      .dateOfBirth("30.11.1999")
+      .email("alexandru.iordan99@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(LocalDateTime.now())
+      .build();
+
+    user2 = User.builder()
+      .firstname("Vlad")
+      .lastname("Loghin")
+      .displayName("gtgmycatisonfire")
+      .password("doomguy2000")
+      .dateOfBirth("23.02.1996")
+      .email("vladloghin00@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(LocalDateTime.now())
+      .build();
+
+    user3 = User.builder()
+      .firstname("Matei")
+      .lastname("Paulet")
+      .displayName("copilcoiot")
+      .password("coiot9000")
+      .dateOfBirth("17.07.1995")
+      .email("mateipaulet1999@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(LocalDateTime.now())
+      .build();
+
+    user1= userRepository.save(user1);
+    user2 = userRepository.save(user2);
+    user3 = userRepository.save(user3);
+
+  }
+
 
 
 
