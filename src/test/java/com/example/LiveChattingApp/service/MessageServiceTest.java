ba87d74d3e6c2c96d3f1b8ba947eb30a6770c213
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-17 19:37:46 +0200

    Initial message service test setup

--- src/test/java/com/example/LiveChattingApp/service/MessageServiceTest.java
+++ src/test/java/com/example/LiveChattingApp/service/MessageServiceTest.java
@@ -1,4 +1,129 @@
 package com.example.LiveChattingApp.service;
 
+
+import com.example.LiveChattingApp.chat.*;
+import com.example.LiveChattingApp.message.Message;
+import com.example.LiveChattingApp.message.MessageRepository;
+import com.example.LiveChattingApp.message.MessageService;
+import com.example.LiveChattingApp.user.User;
+import com.example.LiveChattingApp.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
+@Testcontainers
 public class MessageServiceTest {
+
+  @Mock
+  private ChatRepository chatRepository;
+
+  @Mock
+  private MessageRepository messageRepository;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private ChatMapper mapper;
+
+  @Mock
+  private Authentication authentication;
+
+  @InjectMocks
+  private MessageService messageService;
+
+  private User user1;
+  private User user2;
+  private User user3;
+  private Chat directChat;
+  private Chat groupChat;
+
+  private final LocalDateTime testTime = LocalDateTime.now();
+  private Message message;
+
+
+  @BeforeEach
+  void setUp() {
+    user1 = User.builder()
+      .firstname("Alexandru")
+      .lastname("Iordan")
+      .displayName("Jordan299")
+      .password("alunemari1234")
+      .dateOfBirth("30.11.1999")
+      .email("alexandru.iordan99@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(testTime)
+      .build();
+
+    user1.setId("1");
+
+    user2 = User.builder()
+      .firstname("Vlad")
+      .lastname("Loghin")
+      .displayName("gtgmycatisonfire")
+      .password("doomguy2000")
+      .dateOfBirth("23.02.1996")
+      .email("vladloghin00@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(testTime)
+      .build();
+    user2.setId("2");
+
+    user3 = User.builder()
+      .firstname("Matei")
+      .lastname("Paulet")
+      .displayName("copilcoiot")
+      .password("coiot9000")
+      .dateOfBirth("17.07.1995")
+      .email("mateipaulet1999@gmail.com")
+      .accountLocked(false)
+      .enabled(true)
+      .createdDate(testTime)
+      .build();
+    user3.setId("3");
+
+    directChat = Chat.builder()
+      .id(1L)
+      .type(ChatType.DIRECT)
+      .creator(user1)
+      .participants(Set.of(user1, user2))
+      .lastReadTimestamps(new HashMap<>())
+      .messages(new ArrayList<>())
+      .build();
+
+    groupChat = Chat.builder()
+      .id(2L)
+      .name("Test Group")
+      .type(ChatType.GROUP)
+      .creator(user1)
+      .participants(Set.of(user1, user2, user3))
+      .adminUserIds(Set.of("1"))
+      .lastReadTimestamps(new HashMap<>())
+      .messages(new ArrayList<>())
+      .build();
+
+
+
+
+  }
+
+
+
+
+
+
+
+
 }
