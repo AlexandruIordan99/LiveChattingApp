2eb8a6f2705b459499e7142e52a395e21fd71d66
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-07-18 18:11:40 +0200

    Added constructors

--- src/main/java/com/example/LiveChattingApp/message/MessageInputDTO.java
+++ src/main/java/com/example/LiveChattingApp/message/MessageInputDTO.java
@@ -2,11 +2,15 @@ package com.example.LiveChattingApp.message;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class MessageInputDTO {
   @NotBlank
   private String content;
